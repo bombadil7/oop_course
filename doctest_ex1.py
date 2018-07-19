72312cac16ec88e08c17a2590eb346b8878f480f
Andrei Kniazev <akniazev@gmail.com>
2018-07-19 22:26:18 +0000

    Add doctest notes.

---
+++ doctest_ex1.py
@@ -0,0 +1,22 @@
+def mul(a, b):
+    """
+    >>> mul(2, 3)
+    6
+    >>> mul('a', 2)
+    'aa'
+    """
+    return a * b
+
+def add(a, b):
+    """
+    >>> add(2, 3)
+    5
+    >>> add('a', 'b')
+    'ab'
+    """
+    return a + b
+
+if __name__ == "__main__":
+    import doctest
+#    print(doctest.__file__)
+    doctest.testmod()
