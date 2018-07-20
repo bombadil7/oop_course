e4ff8372ded480c975ca8f32f45bc9d24f908780
Andrei Kniazev <akniazev@gmail.com>
2018-07-20 14:37:22 +0000

    Add exceptions to factorize and change test var to x.

--- test_fact.py
+++ test_fact.py
@@ -5,50 +5,55 @@ def factorize(x):
         :type x: int,>=0
         :rtype: tuple[N],N>0
     """
-    pass
+    if not isinstance(x, int):
+        raise TypeError
+
+    if not x >= 0:
+        raise ValueError 
+
 
 class TestFactorize(unittest.TestCase):
     def test_wrong_types_raise_exception(self):
-        with self.subTest(i=1):
+        with self.subTest(x=1):
             self.assertRaises(TypeError, factorize, 'Hello',
                     msg='String argument not raising TypeError')
-        with self.subTest(i=1):
+        with self.subTest(x=1):
             self.assertRaises(TypeError, factorize, 1.5,
                     msg='Float argument not raising TypeError')
 
     def test_negative(self):
-        with self.subTest(i=1):
+        with self.subTest(x=1):
             self.assertRaises(ValueError, factorize, -1)
-        with self.subTest(i=2):
+        with self.subTest(x=2):
             self.assertRaises(ValueError, factorize, -10)
-        with self.subTest(i=3):
+        with self.subTest(x=3):
             self.assertRaises(ValueError, factorize, -100)
 
     def test_zero_and_one_cases(self):
-        with self.subTest(i=0):
+        with self.subTest(x=0):
             self.assertEqual(factorize(0), (0,))
-        with self.subTest(i=1):
+        with self.subTest(x=1):
             self.assertEqual(factorize(1), (1,))
 
     def test_simple_numbers(self):
-        with self.Subtest(i=1):
+        with self.subTest(x=1):
             self.assertEqual(factorize(3), (3,))
-        with self.Subtest(i=2):
+        with self.subTest(x=2):
             self.assertEqual(factorize(13), (13,))
-        with self.Subtest(i=3):
+        with self.subTest(x=3):
             self.assertEqual(factorize(29), (29,))
 
     def test_two_simple_multipliers(self):
         cases = [(6, (2, 3)), (26, (2, 13)), (121, (11, 11))]
         for n, fact_n in cases:
-            with self.Subtest(i=n):
+            with self.subTest(x=n):
                 self.assertEqual(factorize(n), fact_n)
 
     def test_many_multiplyers(self):
         cases = [(1001, (7, 11, 13)), 
                 (9699690, (2, 3, 5, 7, 11, 13, 17, 19))]
         for n, fact_n in cases:
-            with self.Subtest(i=n):
+            with self.subTest(x=n):
                 self.assertEqual(factorize(n), fact_n)
 
 
